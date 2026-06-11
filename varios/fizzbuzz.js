72d76bdccf895e23cf75d7620aa3b9cb425101dc
Javier Guerra <javguerra@badared.com>
2026-06-11 18:19:23 +0200

    Refactor FizzBuzz implementation to use array

--- varios/fizzbuzz.js
+++ varios/fizzbuzz.js
@@ -8,21 +8,16 @@
 
 const START = 1;
 const END = 100; // START > END
-let result = '0';
-let x3, x5;
 
-for (let i = START; i <= END; i++)
-{
-  result += ', ';
-  x3 = i % 3 == 0;
-  x5 = i % 5 == 0;
+const result = [];
 
-  if (!x3 && !x5)
-    result += i;
-  else {
-    if (x3) result += 'fizz';
-    if (x5) result += 'buzz';
-  }
-} // end for()
+for (let i = START; i <= END; i++) {
+  let value = '';
 
-console.log (result);
+  if (i % 3 === 0) value += 'fizz';
+  if (i % 5 === 0) value += 'buzz';
+
+  result.push(value || i);
+} // end for
+
+console.log(result.join(', '));
